d32225116acd4cbef34b15c7189f0de1bc47da5f
KaliuzhnaMariia <124194241+KaliuzhnaMariia@users.noreply.github.com>
2023-09-12 15:34:08 +0300

    Task 1(variant 1)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Lab 1 Java");
-        System.out.println("Test for Lab1");
-        System.out.println("Demo");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Task 1");
+
+        System.out.println("First version(input and output float): ");
+        System.out.print("Enter the a (float):");
+        double fa = input.nextDouble();
+        System.out.print("Enter the b (float):");
+        double fb = input.nextDouble();
+        double rez1 = (fa * fb - (fa + fb) * (fa - fb))/(Math.pow(fb, 4) + Math.pow(fa, 3)) + 5 * fb;
+        System.out.println("Result for float: " + rez1);
+
+
     }
 }
