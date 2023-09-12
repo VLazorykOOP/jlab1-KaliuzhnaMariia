30236969a4591a0a3f916f9ca9cf085ba48f63f0
KaliuzhnaMariia <124194241+KaliuzhnaMariia@users.noreply.github.com>
2023-09-12 16:34:07 +0300

    Task 1(update for result func and ver. 3) final ver

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,28 @@ public class Main {
         System.out.println("Version 1 (float in and out)");
         double fa = Input("fa", input);
         double fb = Input("fb", input);
-        double rez1 = (fa * fb - (fa + fb) * (fa - fb))/(Math.pow(fb, 4) + Math.pow(fa, 3)) + 5 * fb;
+        double rez1 = Res(fa, fb);
         System.out.println("Result for float: " + rez1);
 
         System.out.println("Version 2 (int in and float out)");
         int ia = (int) Input("ia", input);
         int ib = (int) Input("ib", input);
-        double rez2 = (ia * ib - (ia + ib) * (ia - ib))/(Math.pow(ib, 4) + Math.pow(ia, 3)) + 5 * ib;
+        double rez2 = Res(ia, ib);
         System.out.println("Result for float: " + rez2);
+
+        System.out.println("Version 3 (float in and int out)");
+        double da = Input("da", input);
+        double db = Input("db", input);
+        int rez3 = (int) Res(da, db);
+        System.out.println("Result for int: " + rez3);
     }
     //Input function
     private static double Input(String v, Scanner input){
         System.out.print("Enter the " + v + "(float): ");
         return input.nextDouble();
+    }
+    //Result function
+    private static double Res(double a,double b){
+        return ((a * b - (a + b) * (a - b))/(Math.pow(b, 4) + Math.pow(a, 3)) + 5 * b);
     }
 }
