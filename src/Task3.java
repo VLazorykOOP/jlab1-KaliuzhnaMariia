5fc875b136c849b6042c60e582da13e88fe041f2
KaliuzhnaMariia <124194241+KaliuzhnaMariia@users.noreply.github.com>
2023-09-15 16:13:38 +0300

    Task 2 final ver

--- src/Task3.java
+++ src/Task3.java
@@ -20,7 +20,7 @@ public class Task3 {
         return a;
     }
     //Function for task changes
-    static void Task(int[][] a){
+    static int[][] Task(int[][] a){
         int maxr = 0;
         int maxel = a[0][0];
         int[][] rez = new int[a.length][a.length];
@@ -32,11 +32,30 @@ public class Task3 {
                 }
             }
         }
-        System.out.println("Result: ");
+        for (int i = 0; i < a.length; i++){
+            int newr = (i - maxr + a.length) % a.length;
+            for (int j = 0; j < a.length; j++){
+                rez[newr][j] = a[i][j];
+            }
+        }
+        return rez;
+    }
+    //Output function
+    static void Print(int[][] a){
+        for (int i = 0; i < a.length; i++){
+            for (int j = 0; j < a.length; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         System.out.println("Task 3 (12)");
         int[][] a = Input();
-        Task(a);
+        Print(a);
+        a = Task(a);
+        System.out.println("Result: ");
+        System.out.println();
+        Print(a);
     }
 }
